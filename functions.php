f72e8f2625c36d129355e8ed177bb7b8f3bf02f7
Stefano-Bernini <stefanobernini26@gmail.com>
2023-07-06 00:59:36 +0200

    milestone 2 completata. ESERCIZIO COMPLETATO

--- functions.php
+++ functions.php
@@ -1,3 +1,17 @@
 <?php
+    if(isset($_GET['psw-length']) && $_GET['psw-length'] != '' ){
+        function randomPasswordGen() {
+            $pswLength = $_GET['psw-length'];
 
+            $allowedChar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+            $pass = [];
+            $charLength = strlen($allowedChar) -1;
+            for ($i = 0; $i < $pswLength; $i++) {
+                $n = rand(0, $charLength);
+                $pass[] = $allowedChar[$n];
+            }
+            return implode($pass);
+        }
+   
+    }
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-    if(isset($_GET['psw-length']) && $_GET['psw-length'] != '' ){
-        function randomPasswordGen() {
-            $pswLength = $_GET['psw-length'];
-
-            $allowedChar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-            $pass = [];
-            $charLength = strlen($allowedChar) -1;
-            for ($i = 0; $i < $pswLength; $i++) {
-                $n = rand(0, $charLength);
-                $pass[] = $allowedChar[$n];
-            }
-            return implode($pass);
-        }
-   
-    }
+    include 'functions.php'
 ?>
 
 
